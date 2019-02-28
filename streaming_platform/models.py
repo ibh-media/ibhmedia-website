dbfbf56810365b96ec02c5856059ad8ce668d43a
Giovanni Pecchio <gp30@rice.edu>
2019-02-27 19:23:10 -0600

    added model for movies

--- streaming_platform/models.py
+++ streaming_platform/models.py
@@ -3,4 +3,9 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+class Movie(models.model):
+    title = models.CharField()
+    duration = models.TimeField()
+    year = models.DateField()
+    video_url = models.URLField()
+    #add thumbnail
