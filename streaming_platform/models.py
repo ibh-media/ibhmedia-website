16d32aec5c4336f06a8d51a2478d11af0ee21223
Giovanni Pecchio <gp30@rice.edu>
2019-02-27 19:48:35 -0600

    created migration for movies model + added streaming_platform in INSTALLED_APPS + migrated the new migration

--- streaming_platform/models.py
+++ streaming_platform/models.py
@@ -3,9 +3,16 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-class Movie(models.model):
-    title = models.CharField()
-    duration = models.TimeField()
-    year = models.DateField()
-    video_url = models.URLField()
+class Movie(models.Model):
+    title = models.CharField(max_length=200)
+    duration = models.TimeField(auto_now=False, auto_now_add=False)
+    year = models.DateField(auto_now=False, auto_now_add=False)
+    video_url = models.URLField(max_length=200)
     #add thumbnail
+
+'''
+===== IMPORTANT =====
+Every time you change/create a model:
+    - python manage.py makemigrations
+    - python manage.py migrate
+'''
