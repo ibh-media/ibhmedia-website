abc6d08f6c13ecac3efcfa40061d284816a6432c
Giovanni Pecchio <gp30@rice.edu>
2019-02-27 20:10:34 -0600

    changed movie url variable name from video_url --> url

--- streaming_platform/models.py
+++ streaming_platform/models.py
@@ -3,16 +3,19 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-class Movie(models.Model):
-    title = models.CharField(max_length=200)
-    duration = models.TimeField(auto_now=False, auto_now_add=False)
-    year = models.DateField(auto_now=False, auto_now_add=False)
-    video_url = models.URLField(max_length=200)
-    #add thumbnail
-
 '''
 ===== IMPORTANT =====
 Every time you change/create a model:
     - python manage.py makemigrations
     - python manage.py migrate
 '''
+
+class Movie(models.Model):
+    title = models.CharField(max_length=200)
+    duration = models.TimeField(auto_now=False, auto_now_add=False)
+    year = models.DateField(auto_now=False, auto_now_add=False)
+    url = models.URLField(max_length=200)
+    #add thumbnail
+
+    def __str__(self):
+        return self.title
