b75b2a56c5f1a565776f29876db100c14170a4a9
Giovanni Pecchio <gp30@rice.edu>
2019-02-27 20:42:40 -0600

    changed duration to text field + added slug to movies. Added templating to display movies in movie index page

--- streaming_platform/models.py
+++ streaming_platform/models.py
@@ -12,10 +12,11 @@ Every time you change/create a model:
 
 class Movie(models.Model):
     title = models.CharField(max_length=200)
-    duration = models.TimeField(auto_now=False, auto_now_add=False)
+    duration = models.CharField(max_length=8)
     year = models.DateField(auto_now=False, auto_now_add=False)
     url = models.URLField(max_length=200)
-    #add thumbnail
+    slug = models.SlugField(default='not_found')
+    # add thumbnail
 
     def __str__(self):
         return self.title
--- streaming_platform/views.py
+++ streaming_platform/views.py
@@ -4,6 +4,9 @@ from django.http import HttpResponse
 
 from django.shortcuts import render
 
+# Import models
+from .models import Movie
+
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
@@ -12,7 +15,8 @@ def tv(request):
     return render(request, 'tv.html')
 
 def movies(request):
-    return render(request, 'movies.html')
+    movies = Movie.objects.all().order_by('year') # order by any variable in Movie class from models 
+    return render(request, 'movies.html', {'movies': movies})
 
 def videos(request):
     return render(request, 'videos.html')
