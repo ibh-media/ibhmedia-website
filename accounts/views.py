b11e58773fae6c9e3b3dff52728e394ce2878c5e
Giovanni Pecchio <gp30@rice.edu>
2019-03-05 21:00:14 -0600

    added non-styled sign up form

--- accounts/views.py
+++ accounts/views.py
@@ -1,14 +1,13 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
+from django.contrib.auth.forms import UserCreationForm
 
 from django.shortcuts import render
 
 # Create your views here.
-def index(request):
-    return render(request, 'index.html')
-    
 def signup(request):
-    return render(request, 'signup.html')
+    form = UserCreationForm()
+    return render(request, 'signup.html', {'form': form})
 
 def login(request):
     return render(request, 'login.html')
