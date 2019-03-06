64d51a9f3109e63895e14a5ec9e60e4ff97c8766
Giovanni Pecchio <gp30@rice.edu>
2019-03-05 21:43:00 -0600

    styled and working sign up form

--- accounts/views.py
+++ accounts/views.py
@@ -2,11 +2,18 @@
 from __future__ import unicode_literals
 from django.contrib.auth.forms import UserCreationForm
 
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
 def signup(request):
-    form = UserCreationForm()
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            # log user in
+            return redirect('movies')
+    else:
+        form = UserCreationForm()
     return render(request, 'signup.html', {'form': form})
 
 def login(request):
