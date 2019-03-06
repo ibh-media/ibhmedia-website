fca8d6d43eecd8a2f6c51c9cd01d369027066dbd
Giovanni Pecchio <gp30@rice.edu>
2019-03-06 10:41:01 -0600

    added log in functionalities

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,8 +4,8 @@ from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from . import views
 
 urlpatterns = [
-    url(r'^signup/', views.signup, name='signup'),
-    url(r'^login/', views.login, name='login'),
+    url(r'^signup/', views.signup_view, name='signup'),
+    url(r'^login/', views.login_view, name='login'),
 ]
 
 urlpatterns += staticfiles_urlpatterns()
--- accounts/views.py
+++ accounts/views.py
@@ -1,26 +1,30 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import login
 
 from django.shortcuts import render, redirect
 
 # Create your views here.
-def signup(request):
+def signup_view(request):
     if request.method == 'POST':
         form = UserCreationForm(request.POST)
         if form.is_valid():
-            form.save()
+            user = form.save()
             # log user in
+            login(request, user)
             return redirect('movies')
     else:
         form = UserCreationForm()
     return render(request, 'signup.html', {'form': form})
 
-def login(request):
+def login_view(request):
     if request.method == 'POST':
         form = AuthenticationForm(data = request.POST)
         if form.is_valid():
             #log in user
+            user = form.get_user()
+            login(request, user)
             return redirect('movies')
     else:
         form = AuthenticationForm()
