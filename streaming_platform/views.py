1d48b7c9b63677bee26f170045c8c1a82e7cc5d2
Giovanni Pecchio <gp30@rice.edu>
2019-03-06 21:09:02 -0600

    added required login for viewing content pages

--- streaming_platform/views.py
+++ streaming_platform/views.py
@@ -4,6 +4,8 @@ from django.http import HttpResponse
 
 from django.shortcuts import render
 
+from django.contrib.auth.decorators import login_required
+
 # Import models
 from .models import TV_channel
 from .models import Movie
@@ -14,18 +16,22 @@ from .models import Podcast
 def index(request):
     return render(request, 'index.html')
 
+@login_required(login_url="/accounts/login")
 def tv(request):
     tv_channels = TV_channel.objects.all().order_by('live') # order by any variable in Movie class from models 
     return render(request, 'tv.html', {'tv_channels': tv_channels})
 
+@login_required(login_url="/accounts/login")
 def movies(request):
     movies = Movie.objects.all().order_by('release_date') 
     return render(request, 'movies.html', {'movies': movies})
 
+@login_required(login_url="/accounts/login")
 def videos(request):
     videos = Video.objects.all().order_by('release_date')
     return render(request, 'videos.html', {'videos': videos})
 
+@login_required(login_url="/accounts/login")
 def podcasts(request):
     podcasts = Podcast.objects.all().order_by('release_date') 
     return render(request, 'podcasts.html', {'podcasts': podcasts})
