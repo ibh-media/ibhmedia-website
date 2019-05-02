449cca7805f1c647b12b3c3f63a868d2f4e44eb4
Giovanni Pecchio <gp30@rice.edu>
2019-05-02 20:38:42 +0200

    added date range filter; TODO (extra): style the range so it's next to each other

--- streaming_platform/filters.py
+++ streaming_platform/filters.py
@@ -4,4 +4,8 @@ import django_filters
 class MovieFilter(django_filters.FilterSet):
     class Meta:
         model = Movie
-        fields = ['title', 'director', 'release_date', ] 
+        fields = {
+            'title': ['contains'],
+            'director': ['contains'],
+            'release_date': ['year__lt', 'year__gt'],
+        }
